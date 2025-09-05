2497a8e192e1f89ecd5655f1b1c7512d54a9d455
pablospena <4pablosuarezpena@gmail.com>
2025-09-05 22:57:23 +0200

    3 commit

--- Basico/00-helloworld.js
+++ Basico/00-helloworld.js
@@ -1,3 +1,27 @@
+// Esto es un comentario simple.
+
+/* y esto
+es un comentario
+que ya no es simple.*/
+
 console.log("¡Hola, JavaScript!")
 console.log('¡Hola, JavaScript!')
 console.log(`¡Hola, JavaScript!`)
+
+
+console.log("Numero 5 en cadena de texto")
+console.log("5")
+console.log("Numero 5")
+console.log(5)
+console.log("Numero 5 + 2 =")
+console.log(5 + 2)
+console.log("Numero 5 - 2 =")
+console.log(5 - 2)
+console.log("Numero 5 - 2 + 1=")
+console.log(5 - 2 + 1)
+console.log("Numero 5 * 2 =")
+console.log(5 * 2)
+console.log("Numero 5 / 2 =")
+console.log(5 % 2)
+console.log("Numero 5 ^ 2 =")
+console.log(5 ** 2)
