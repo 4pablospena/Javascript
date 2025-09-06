fcbb817f6436554c7734b26e034ad549f039d6a6
pablospena <4pablosuarezpena@gmail.com>
2025-09-06 10:11:01 +0200

    variables-commit

--- Basico/01-variables.js
+++ Basico/01-variables.js
@@ -1,5 +1,28 @@
-// Variables
+// Variables.
 
-
+// Var: No crear hoy en día!
+// creamos la variable y la inicializamos con un valor.
 var holaMundo = "¡Hola, mundo!"
 console.log(holaMundo)
+
+// Al ser una cadena de texto, podemos cambiar el valor de la variable
+holaMundo = "¡Hola de nuevo, mundo!"
+console.log(holaMundo)
+
+
+// let
+// creamos la variable y la inicializamos con un valor.
+let holaMundo2 = "¡Hola, mundo!"
+console.log(holaMundo2)
+
+// Al ser una cadena de texto, podemos cambiar el valor de la variable
+holaMundo2 = "¡Hola de nuevo, mundo!"
+console.log(holaMundo2)
+
+// Const
+
+const holaMundo3 = "¡Hola, mundo!"
+console.log(holaMundo3)
+
+/* Al ser una cadena de texto, no podemos cambiar el valor de la variable
+ el valos que le damos será el asignado.*/
