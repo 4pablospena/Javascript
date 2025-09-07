7b6eebebcc0b352fb8ade9e48a4e0f0366747fc8
pablospena <4pablosuarezpena@gmail.com>
2025-09-07 21:33:58 +0200

    op1-commit

---
+++ Basico/04-operators.js
@@ -0,0 +1,43 @@
+// operadores. Relaciones, operaciones con los valores.
+ 
+// Operadores aritméticos.
+
+    let a = 4
+    let b = 2
+
+    console.log( a + b) // Suma
+    console.log( a - b) // Resta
+    console.log( a * b) // multiplicación
+    console.log( a / b) // división
+
+    console.log( a % b) // modulo,  resto de división
+    console.log( a ** b) // exponente
+
+    a++  // autoincremento
+    console.log(a)
+
+    b--  // autodecremento
+    console.log(b)
+
+// Operadores de asignación.
+
+    let myVariable = 2  
+    console.log (myVariable)
+
+    myVariable += 5  // auto suma 5 = 7
+    console.log (myVariable)
+
+    myVariable -= 1  // auto resta 1 = 6
+    console.log (myVariable)
+
+    myVariable *= 2  // auto multiplicación 2 = 12 
+    console.log (myVariable)
+
+    myVariable /= 2  // auto división 2 = 6
+    console.log (myVariable)
+
+    myVariable %= 4  // auto modulo 4 = 2
+    console.log (myVariable)
+
+    myVariable **= 2  // auto exponente 2 = 4
+    console.log (myVariable)
