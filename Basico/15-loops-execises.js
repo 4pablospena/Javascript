f9f82a55a58c7144d7e3eefc632a95a4d4986bbd
pablospena <4pablosuarezpena@gmail.com>
2025-09-16 23:09:38 +0200

    loops-exer1-commit

--- Basico/15-loops-execises.js
+++ Basico/15-loops-execises.js
@@ -1,12 +1,35 @@
 // NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios.
 
-// 1. Crea un bucle que imprima los nmeros del 1 al 20.
+// 1. Crea un bucle que imprima los números del 1 al 20.
+
+for (let n = 1; n <= 20; n++ ) {
+    console.log(`n = ${n}` )
+}
 
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado.
 
+let suma = 0
+
+for (let n = 1; n <= 100; n++ ) {
+    suma += n
+} console.log( `suma = ${suma}` )
+
+
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50.
 
+for (let n = 1; n <= 50; n++ ){
+    if (n % 2 === 0 ){
+        console.log (`${n} es un numero par`)
+    }
+} 
+
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola.
+
+let nombres = ["Pablo", "Juan", "Nacho", "Nana", "Lucas" ]
+
+for (let valor of nombres) {
+    console.log(valor)
+}
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto.
 
